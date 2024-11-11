ceb7d5a87e69e6df778d98ad8e92b00cb13fa82f
Paul Hübner <phubner@kth.se>
2024-11-11 15:50:04 +0100

    Update entire model with lagged quality.

--- notebooks/functions/util.py
+++ notebooks/functions/util.py
@@ -187,7 +187,7 @@ def plot_air_quality_forecast(city: str, street: str, df: pd.DataFrame, file_pat
 
     day = pd.to_datetime(df['date']).dt.date
     # Plot each column separately in matplotlib
-    ax.plot(day, df['predicted_pm25'], label='Predicted PM2.5', color='red', linewidth=2, marker='o', markersize=5, markerfacecolor='blue')
+    ax.plot(day, df['predicted_pm25'], label='Vorhersage PM2.5', color='red', linewidth=2, marker='o', markersize=5, markerfacecolor='blue')
 
     # Set the y-axis to a logarithmic scale
     ax.set_yscale('log')
@@ -196,19 +196,19 @@ def plot_air_quality_forecast(city: str, street: str, df: pd.DataFrame, file_pat
     ax.set_ylim(bottom=1)
 
     # Set the labels and title
-    ax.set_xlabel('Date')
-    ax.set_title(f"PM2.5 Predicted (Logarithmic Scale) for {city}, {street}")
-    ax.set_ylabel('PM2.5')
+    ax.set_xlabel('Datum')
+    ax.set_title(f"Reutlingen Zaisentalstraße PM2.5 Vorhersage (Logarithmische Skala)")
+    ax.set_ylabel('PM2.5 (je weniger desto besser)')
 
     colors = ['green', 'yellow', 'orange', 'red', 'purple', 'darkred']
-    labels = ['Good', 'Moderate', 'Unhealthy for Some', 'Unhealthy', 'Very Unhealthy', 'Hazardous']
+    labels = ['Gut', 'Mittelmäßig', 'Teilweise Ungesund', 'Ungesund', 'Sehr Ungesund', 'Gefährlich']
     ranges = [(0, 49), (50, 99), (100, 149), (150, 199), (200, 299), (300, 500)]
     for color, (start, end) in zip(colors, ranges):
         ax.axhspan(start, end, color=color, alpha=0.3)
 
     # Add a legend for the different Air Quality Categories
     patches = [Patch(color=colors[i], label=f"{labels[i]}: {ranges[i][0]}-{ranges[i][1]}") for i in range(len(colors))]
-    legend1 = ax.legend(handles=patches, loc='upper right', title="Air Quality Categories", fontsize='x-small')
+    legend1 = ax.legend(handles=patches, loc='upper right', title="Kategorien", fontsize='x-small')
 
     # Aim for ~10 annotated values on x-axis, will work for both forecasts ans hindcasts
     if len(df.index) > 11:
@@ -218,7 +218,7 @@ def plot_air_quality_forecast(city: str, street: str, df: pd.DataFrame, file_pat
     plt.xticks(rotation=45)
 
     if hindcast == True:
-        ax.plot(day, df['pm25'], label='Actual PM2.5', color='black', linewidth=2, marker='^', markersize=5, markerfacecolor='grey')
+        ax.plot(day, df['pm25'], label='Echtwert PM2.5', color='black', linewidth=2, marker='^', markersize=5, markerfacecolor='grey')
         legend2 = ax.legend(loc='upper left', fontsize='x-small')
         ax.add_artist(legend1)
 
